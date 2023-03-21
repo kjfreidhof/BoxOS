0d8d30a8e7f4110e94bfec3b4e9a14a119b88ea6
kjfreidhof <89760217+kjfreidhof@users.noreply.github.com>
2023-03-21 09:45:29 -0500

    Add files via upload

--- Boxos.sh
+++ Boxos.sh
@@ -1,21 +1,44 @@
 #!/bin/sh
+
+# Author = kjfreidhof
+
+# Created = /3/21/23
+
+# script = Boxos.sh
+
+#License = GPL3
+
+# about = own personal script for my window manager and linux distros slash bsd distro that i use this script is for me and me only
+# but others can use it as well if you want it is Free software and licensed under the GPL3 license 
+
+# my packages being installed by the opreating systems i use 
 bsd="sudo pkg install bspwm polybar sxhkd nitrogen picom terminator"
+pkg1="sudo pkg install firefox flameshot"
 arch="sudo pacman -S bspwm terminator polybar sxhkd nitrogen picom -y"
+pkg2="sudo pacman -S firefox flameshot dmenu obs-studio kdenlive pavucontrol vlc -y"
 fed="sudo dnf install bspwm polybar sxhkd nitrogen picom terminator -y"
+pkg3="sudo dnf install firefox flameshot dmenu obs-studio kdenlive pavucontrol vlc -y"
 Deb="sudo apt install bspwm polybar sxhkd nitrogen picom terminator -y"
+pkg4="sudo apt install firefox flameshot dmenu obs-studio kdenlive pavucontrol vlc -y"
+
+# Asks user what are you using 
+# and based on what they are using it will install the packages 
+# based on the opreating system they choose so if they choose arch it will install for arch etc.
 echo "What are you using?"
 echo "1, Freebsd"
 echo "2, Arch"
 echo "3, Fedora"
 echo "4, Ubuntu/Debian"
 
-read -rp CHOICE
+read CHOICE
 
 if [ "$CHOICE" = 1 ]; then
 	for i in 5
 	do
 		sleep $i
 		$bsd
+		sleep $i
+		$pkg1
 		continue 
 	
 	done	
@@ -24,8 +47,11 @@ elif [ "$CHOICE" = 2 ]; then
 	for i in 5
 	do
 		sleep $i 
-		$arch 
-		continue 
+		$arch
+	        sleep $i
+		$pkg2 	
+		continue
+	       	
 
 	done
 
@@ -35,6 +61,8 @@ elif [ "$CHOICE" = 3 ]; then
 	do
 		sleep $i
 		$fed
+		sleep $i
+		$pkg3
 		continue 
 
 	done
@@ -45,6 +73,8 @@ elif [ "$CHOICE" = 4 ]; then
 	do
 		sleep $i
 		$deb
+		sleep $i 
+		$pkg4
 		continue 
 
 	done
@@ -58,6 +88,7 @@ else
 
 fi
 
+# copies everything to the ~/.config/ directory
 cp * ~/.config/
 exit
 
--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# Install script for Bspwm 
+# Boxos
+
+## This is my own personal script for the opreating systems that i use this is for me and me only but others can use it and fork it
+## Do what ever they want but note this
+
+This is based on the applications i use and i use daily what does this mean well basically its meant for me and me alone but others 
+can use it if they want but it is not for others i am adding applications based on what i want in the script not based on what others want however this is licensed under the GPL3 license so you can use it if you want fork it make your own version of it please I 
+encourage you with that out of the way lets move on
+
+## Optional requirement 
+
+You dont have to but it is reccomended that you use git or if you have it installed if not that is fine i have made a zip binary in 
+the releases that you can go and Download here 
+
+
+## How to use this script 
+
+- option 1 
+
+```
+git clone https://github.com/kjfreidhof/BoxOS.git
+```
+then
+
+
+```
+chmod +x Boxos.sh 
+```
+then
+
+```
+./Boxos.sh
+```
+- option 2 
+
+use the zip binary ive made here 
+
+## Then you are done :)
+
+
+
+
